d4456671fb9d2b1739bb26032ccccdac9b67ed8c
Eric George <ericgeo324@gmail.com>
2023-10-07 04:54:29 -0400

    Animations and fixed and camera implemented

--- Player.gd
+++ Player.gd
@@ -1,13 +1,43 @@
 extends CharacterBody2D
 
-@export var speed = 400
-@onready var sprite = get_node("AnimatedSprite2D")
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-	print(input_direction)
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	get_input()
-	sprite.play("Walk")
-	move_and_slide()
+	var velocity = Vector2.ZERO # The player's movement vector.
+
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+		print(velocity.x)
+		print(velocity.y)
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+
+	if velocity.x > 0:
+		$AnimatedSprite2D.animation = "WalkRight"
+	elif velocity.x<0:
+		$AnimatedSprite2D.animation = "WalkLeft"
+	
+
+	if velocity.y > 0:
+		$AnimatedSprite2D.animation = "WalkFor"
+	elif velocity.y<0:
+		$AnimatedSprite2D.animation = "WalkBack"
+
