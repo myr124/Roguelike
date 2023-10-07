c2cf45cb14c4dee5f2da0adf039aa07a76203758
jcamp50 <7jacampbell@gmail.com>
2023-10-07 00:59:44 -0400

    store

---
+++ Store.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
--- main_menu.gd
+++ main_menu.gd
@@ -7,3 +7,7 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+	
+
+func _on_store_button_pressed():
+	get_tree().change_scene_to_file("res://Store.tscn")
